6e95f56a37ba087d53771b33ee6b5b39b51d02b9
JoieAli <znjsjcy@126.com>
2019-05-04 22:25:29 +0800

    modify

--- getData.py
+++ getData.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import pandas_datareader as pdr
-alibaba = pdr.get_data_yahoo("BABA")
-print(alibaba.head(5))
-print(alibaba.size)
-print(alibaba.shape)
-print(alibaba.info)
+import numpy as np
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+
+# start = datetime(2018, 5, 1)
+# alibaba = pdr.get_data_yahoo("BABA", start=start)
+# alibaba.to_csv("/home/joiejiang/PycharmProjects/stock/alibaba")
+# alibaba['Adj Close'].plot(kind='line', legend=True)
+# print(alibaba.head(5))
+
+alibaba = pd.read_csv("/home/joiejiang/PycharmProjects/stock/alibaba")
+print(type(alibaba))
+closeprice = alibaba['Close']
+print(closeprice[0], type(closeprice))
+
+
